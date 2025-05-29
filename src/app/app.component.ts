c3edf8c5b3ec204fef423b439461b1a5dc7e63a7
brahana <brahanarama@gmail.com>
2025-05-29 07:45:42 +0530

    completed till CompletedItems and full CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-simple-crud';
-  name="Brahana"
+  name="";
+  pendingItems:string[]=[];
+  editIndex:number|null=null;
+  deleteIndex:number|null=null;
+  //completedIndex:number|null=null;
+  completedItems:string[]=[];
+
+  save()
+  {
+  if(this.editIndex!=null)
+  {
+    this. pendingItems=this.pendingItems.map((val,i)=>{
+      if(this.editIndex==i)
+      {
+        val=this.name;
+      }
+      return val;
+    })
+  }
+  else
+  { 
+     this.pendingItems.push(this.name)
+  }
+  this.editIndex=null;
+  // Reset the input field after saving
+  this.name="";
+}
+
+  
+  
+
+  edit(editIndex:number)
+  {
+    this.editIndex=editIndex
+    const editData=this.pendingItems.find((val,i)=>{
+      return editIndex==i;
+    })
+    if(editData)
+          this.name=editData;
+  }
+
+  delete(deleteIndex:number)
+{
+ this.pendingItems=this.pendingItems.filter((val,i)=>{
+  return deleteIndex!=i;
+ })
+}
+
+complete(completedIndex:number)
+{
+  const completedItem=this.pendingItems.find((val,i)=>{
+ 
+    return completedIndex==i
+  })
+  this.pendingItems=this.pendingItems.filter((val,i)=>{
+    return completedIndex!=i;
+  })
+  if(completedItem)
+  this.completedItems.push(completedItem)
+console.log(this.completedItems);
 
 }
+}
+
+
